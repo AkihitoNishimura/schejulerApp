24b92fd1cca4c9ef5ee2a479e53d93ac3cab46c2
AkihitoNishimura <plumizreop.3@gmail.com>
2020-09-18 21:10:46 +0900

    Initial Commit

---
+++ schejulerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  schejulerApp
+//
+//  Created by 西村映人 on 2020/09/18.
+//  Copyright © 2020 西村映人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
